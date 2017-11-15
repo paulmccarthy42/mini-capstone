8b089bf5ebd2117ab5b3f61a7f0d59c5db3137ee
Paul McCarthy <paul.mccarthy@avant.com>
2017-11-14 20:50:53 -0600

    creating exploratory v2

--- app/controllers/v2/products_controller.rb
+++ app/controllers/v2/products_controller.rb
@@ -1,10 +1,10 @@
-class V1::ProductsController < ApplicationController
+class V2::ProductsController < ApplicationController
   def display_products
     render json: Product.all
   end
 
-  def one_product
-    product = Product.first
+  def one_product(id)
+    product = Product.find_by(id: id)
     render json: product
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   namespace :v1 do
+    get "/product" => "products#one_product"
+    get "/products" => "products#display_products"
+    get "/active_products" => "products#display_active_products"
+    get "/restock" => "products#restock"
+    get "/buy_product" => "products#buy_product"
+  end
+
+  namespace :v2 do
     get "/product/:id" => "products#one_product#(:id)"
     get "/products" => "products#display_products"
     get "/active_products" => "products#display_active_products"

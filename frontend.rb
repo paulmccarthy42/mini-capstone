50f6c9f4bf2a85f6dac9a4a96ca97038183f8d87
Paul McCarthy <paul.mccarthy@avant.com>
2017-11-14 19:01:17 -0600

    frontend improvements

--- frontend.rb
+++ frontend.rb
@@ -1,16 +1,34 @@
 require "unirest"
+require "pp"
 
-puts "Welcome to the PokeMart"
-response =Unirest.get("http://localhost:3000/active_products")
+puts "Welcome to the PokeMart!"
 
-active_products = response.body
-
+gets.chomp
 while true
+  response =Unirest.get("http://localhost:3000/products")
+  active_products = response.body.sort_by{|x| x["id"]}
+  
+  system "clear"
+
+  puts "What would you like to buy?"
   active_products.each do |product|
-    puts "#{product["id"]}. #{product["name"]} cost: #{product["price"]}"
+    puts "#{product["id"]}. #{product["name"]} cost: #{product["price"]} (#{(product["stock"] > 0 ? "" : "Not ") + "In Stock"})"
   end
+  puts ""
   choice = gets.chomp.to_i
   if choice == 0
     break
+  else
+    chosen_product = active_products[choice]
+    pp chosen_product
+    puts "Would you like to buy a #{chosen_product["name"]}, y/n?"
+    input = gets.chomp
+    if input.downcase == 'y'
+      response = Unirest.get()
+      puts "bought"
+    else
+      buts "not bought"
+    end
+    gets.chomp
   end
 end

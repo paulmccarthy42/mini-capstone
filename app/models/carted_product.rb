8f3076441646daf694ddf852782cc5cc9a67e1ed
Paul McCarthy <paul.mccarthy@avant.com>
2017-12-03 13:21:51 -0600

    random validation

--- app/models/carted_product.rb
+++ app/models/carted_product.rb
@@ -1,4 +1,6 @@
 class CartedProduct < ApplicationRecord
+  validates :quantity, numericality: {greater_than_or_equal_to: 0}
+
   belongs_to :product
   belongs_to :user
   belongs_to :order, optional: true
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
+  validates :subtotal, :tax, :total, numericality: {greater_than_or_equal_to: 0}
+
   belongs_to :user
   has_many :carted_products
 end

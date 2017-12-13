bc3ed5c97617f18047c60333a202bb413c9163fc
Paul McCarthy <paul.mccarthy@avant.com>
2017-12-12 22:28:06 -0600

    random

--- public/index.js
+++ public/index.js
@@ -1,19 +1,6 @@
 /* global axios*/
 var template = document.querySelector("#item");
 var objectHolder = document.querySelector(".row");
-
-function addCard() {
-  var template = document.querySelector("#item");
-
-  var test = template.content.cloneNode(true);
-  var h3tag = test.querySelector("#name");
-  h3tag.innerText = "Hello";
-  objectHolder.appendChild(test);
-  var test1 = template.content.cloneNode(true);
-  objectHolder.appendChild(test1);
-}
-
-// function testCall() {
 axios.get("http://localhost:3000/v3/products").then(function(response) {
   var products = response.data;
   products.forEach(function(product) {
@@ -29,4 +16,3 @@ axios.get("http://localhost:3000/v3/products").then(function(response) {
     objectHolder.appendChild(clone);
   });
 });
-// }
